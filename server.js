140dd0e9cd61cb7cf09a59f2c2f4e65262fa98e0
solm7 <105757874+solm7@users.noreply.github.com>
2022-09-05 15:13:28 -0400

    Added queries to searchDatabase function

--- server.js
+++ server.js
@@ -42,13 +42,48 @@ function searchDatabase() {
           connection
             .promise()
             .query(
+              'SELECT workers.id AS "ID",  workers.first_name, workers.last_name, roles.title AS "Title", department_name AS "Department", roles.salary AS "Salary", CONCAT(mgr.first_name, " ", mgr.last_name) AS "Manager" FROM employees workers LEFT JOIN employees mgr ON workers.manager_id = mgr.id INNER JOIN roles ON workers.role_id = roles.id INNER JOIN departments ON department_id = departments.id ORDER BY workers.id;'
+            )
+            .then(([rows]) => {
+              console.table(rows);
+              searchDatabase();
+            });
+          break;
+
+        case "View all departments":
+          connection
+            .promise()
+            .query(
               'SELECT id AS "ID", department_name AS "Department Name" FROM departments ORDER BY id;'
             )
             .then(([rows]) => {
               console.table(rows);
               searchDatabase();
             });
-          console.log("selected " + answers.enterprise);
+          break;
+
+        case "View all roles":
+          connection
+            .promise()
+            .query(
+              'SELECT roles.id AS "ID", title AS "Roles", salary AS "Salary", department_name AS "Departments"  FROM roles INNER JOIN departments ON roles.department_id = departments.id ORDER BY roles.id;'
+            )
+            .then(([rows]) => {
+              console.table(rows);
+              searchDatabase();
+            });
+          break;
+
+        case "Hire Employee":
+          connection
+            .promise()
+            .execute(
+              'SELECT id AS "ID", title AS "Roles" FROM roles ORDER BY id;'
+            )
+            .then(([rows]) => {
+              console.table(rows);
+              searchDatabase();
+            });
           break;
 
         default:
